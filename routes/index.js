714288cf686c157dfa3a4dcb264defdb17cdd9f5
Jamie1x <jkennedy1x@gmail.com>
2017-01-28 18:51:32 -0500

    bootstrap

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  let currentDate = new Date();
+  res.render('index', { 
+    title: 'Express',
+    date: currentDate.getFullYear()
+  });
+});
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
